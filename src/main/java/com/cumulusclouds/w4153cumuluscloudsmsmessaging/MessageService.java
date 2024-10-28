51e05e7f9c6aa70864b71eec629c4fb579251d6a
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-27 21:43:54 -0400

    build: microservicel logic

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsmessaging/MessageService.java
@@ -0,0 +1,27 @@
+package com.cumulusclouds.w4153cumuluscloudsmsmessaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    public Message sendMessage(Long senderId, Long receiverId, String content) {
+        Message message = new Message();
+        message.setSenderId(senderId);
+        message.setReceiverId(receiverId);
+        message.setContent(content);
+        message.setTimestamp(LocalDateTime.now());
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getMessagesForUser(Long userId) {
+        return messageRepository.findBySenderIdOrReceiverId(userId, userId);
+    }
+}
