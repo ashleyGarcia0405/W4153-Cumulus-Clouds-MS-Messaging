7d3b1d88d3f706df72cee8104205e8c706ce26e5
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-27 21:24:58 -0400

    chore: intial commit

---
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsmessaging/W4153cumuluscloudsmsmessagingApplication.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4153cumuluscloudsmsmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4153cumuluscloudsmsmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W4153cumuluscloudsmsmessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumulusclouds/w4153cumuluscloudsmsmessaging/W4153cumuluscloudsmsmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4153cumuluscloudsmsmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W4153cumuluscloudsmsmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
