2d4177e50a4c111adc24204de71f48667e02838b
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-30 07:02:24 -0400

    build: Attachment and Message JPA repositories

--- src/main/java/com/cumulusclouds/w4153cumuluscloudsmsmessaging/repository/AttachmentRepository.java
+++ src/main/java/com/cumulusclouds/w4153cumuluscloudsmsmessaging/repository/AttachmentRepository.java
@@ -3,12 +3,11 @@ package com.cumulusclouds.w4153cumuluscloudsmsmessaging.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
-
-import com.cumulusclouds.w4153cumuluscloudsmsmessaging.model.Message;
-
 import java.util.List;
 
+import com.cumulusclouds.w4153cumuluscloudsmsmessaging.model.Attachment;
+
 @Repository
-public interface MessageRepository extends JpaRepository<Message, UUID> {
-    List<Message> findBySenderIdOrReceiverId(UUID senderId, UUID receiverId);
+public interface AttachmentRepository extends JpaRepository<Attachment, UUID> {
+    List<Attachment> findByMessageId(UUID messageId);
 }
